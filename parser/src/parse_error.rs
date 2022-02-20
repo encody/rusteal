4db65394cb2d7e75158e3f3e51d409b10341e37a
Jacob <encody@users.noreply.github.com>
2022-02-20 02:46:33 -0700

    adds parse error

---
+++ parser/src/parse_error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ParseError<'a> {
+    #[error("Invalid program name {0}")]
+    InvalidProgramName(&'a str),
+    #[error("Duplicate program name {0}")]
+    DuplicateProgramName(&'a str),
+    #[error("Invalid schema name {0}")]
+    InvalidSchemaName(&'a str),
+    #[error("Duplicate schema name {0}")]
+    DuplicateSchemaName(&'a str),
+}
