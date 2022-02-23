7ada78739281608329cac1c98719a74922100e79
Jacob <encody@users.noreply.github.com>
2022-02-23 14:26:10 -0600

    condition may contain any expression in else branch

--- ast/src/expression/cond.rs
+++ ast/src/expression/cond.rs
@@ -9,7 +9,7 @@ use crate::{
 use super::{Expr, Expression};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Cond(pub Expr, pub Expr, pub Option<Box<Self>>);
+pub struct Cond(pub Expr, pub Expr, pub Option<Expr>);
 
 impl Expression for Cond {
     fn resolve(&self, context: &TypeContext) -> Result<TypeEnum, TypeError> {
@@ -65,11 +65,11 @@ mod tests {
         let prog = Cond(
             Expr::Primitive(Primitive::UInt64(0)),
             Expr::Primitive(Primitive::Byteslice(b"hello".to_vec())),
-            Some(Box::new(Cond(
+            Some(Expr::Cond(Box::new(Cond(
                 Expr::Primitive(Primitive::UInt64(1)),
                 Expr::Primitive(Primitive::UInt64(6)),
                 None,
-            ))),
+            )))),
         );
         println!("{:?}", prog.resolve(&TypeContext::default()));
         println!("{}", prog.compile_raw().unwrap());
