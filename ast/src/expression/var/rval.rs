a15e14ddf259c56d26c27876fc30a25039db4e20
Jacob <encody@users.noreply.github.com>
2022-02-23 14:26:36 -0600

    local var rval should pop stack

--- ast/src/expression/var/rval.rs
+++ ast/src/expression/var/rval.rs
@@ -28,7 +28,7 @@ impl Expression for RVal {
     fn compile(
         &self,
         context: &CompilationContext,
-        _: &mut Vec<String>,
+        prepared_stack: &mut Vec<String>,
     ) -> Result<String, CompilationError> {
         match &self.0 {
             Var::Global(identifier) => Ok(format!(
@@ -37,7 +37,8 @@ impl Expression for RVal {
                     .compile(context, &mut Vec::new())?
             )),
             Var::Local(identifier) => Ok(format!(
-                "{push_identifier}{OP_SEPARATOR}app_local_get", // app_local_get pops 2 elements (second is account identifier), which is why it is typed as a function instead of a simple primitive
+                "{account}{OP_SEPARATOR}{push_identifier}{OP_SEPARATOR}app_local_get", // app_local_get pops 2 elements (second is account identifier), which is why it is typed as a function instead of a simple primitive
+                account = prepared_stack.pop().ok_or(CompilationError::MissingStack)?,
                 push_identifier = Primitive::Byteslice(identifier.as_bytes().to_vec())
                     .compile(context, &mut Vec::new())?
             )),
